81a4cf4123596cfd408589bab7fe14823aa97f06
AmandaKarunathilaka <amandakarunathilaka111@gmail.com>
2024-07-31 10:06:30 +0530

    Add files via upload

---
+++ day01/app/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from .models import Category, Item, Unit
+
+class CategoryFilter(django_filters.FilterSet):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+class ItemFilter(django_filters.FilterSet):
+    class Meta:
+        model = Item
+        fields = ['name', 'category', 'unit']
+        
+class UnitFilter(django_filters.FilterSet):
+    class Meta:
+        model = Unit
+        fields = '__all__'
